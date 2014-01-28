493c5a5a60adaa02cf562c05f051a20f12d67ebf
pimterry <pimterry@gmail.com>
2014-01-28 22:34:41 +0000

    Add an outline of the pipeline model

---
+++ pipeline_notifier/pipeline_model.py
@@ -0,0 +1,47 @@
+class Pipeline:
+    def __init__(self, name, steps, notifier):
+        self.name = name
+        self._steps = steps
+
+        for step in steps:
+            step.add_failure_listener(notifier.on_failure)
+
+        for step, nextStep in zip(steps, steps[1:]):
+            step.add_success_listener(nextStep.add_commit)
+
+        steps[-1].add_success_listener(notifier.on_success)
+
+class BuildStep:
+    def __init__(self, name):
+        self.name = name
+        self.waiting_commits = []
+        self.success_callbacks = []
+        self.failure_callbacks = []
+
+    def add_success_listener(self, callback):
+        self.success_callbacks.append(callback)
+
+    def add_failure_listener(self, callback):
+        self.failure_callbacks.append(callback)
+
+    def add_commit(self, commit):
+        self.waiting_commits.append(commit)
+
+    def succeed(self):
+        tested_commits = self.waiting_commits[:]
+        self.waiting_commits.clear()
+
+        for commit in tested_commits:
+            for callback in self.success_callbacks:
+                callback(commit)
+
+    def fail(self):
+        tested_commits = self.waiting_commits[:]
+        self.waiting_commits.clear()
+
+        for commit in tested_commits:
+            for callback in self.failure_callbacks:
+                callback(commit)
+
+class Commit:
+    pass
---
+++ pipeline_notifier_test/pipeline_model_test.py
@@ -0,0 +1,78 @@
+import unittest
+from unittest.mock import Mock, call
+from pipeline_notifier.pipeline_model import Pipeline, BuildStep, Commit
+
+class PipelineTests(unittest.TestCase):
+    def test_callbacks_are_set_on_build_steps(self):
+        step, notifier = Mock(), Mock()
+
+        Pipeline("pipeline", [step], notifier)
+
+        self.assertEquals(1, len(step.add_success_listener.mock_calls))
+        self.assertEquals(1, len(step.add_failure_listener.mock_calls))
+
+    def test_an_intermediate_successful_step_is_told_to_call_the_next_step(self):
+        step1, step2, notifier = Mock(), Mock(), Mock()
+
+        Pipeline("pipeline", [step1, step2], notifier)
+
+        self.assertEquals(step1.add_success_listener.mock_calls[0], call(step2.add_commit))
+
+    def test_a_final_successful_step_causes_a_notification(self):
+        step1, step2, commit, notifier = Mock(), Mock(), Mock(), Mock()
+
+        Pipeline("pipeline", [step1, step2], notifier)
+
+        success_callback = step2.add_success_listener.call_args[0][0]
+        success_callback(commit)
+
+        notifier.on_success.assert_called_once_with(commit)
+
+    def test_a_failing_step_causes_a_notification(self):
+        step1, step2, commit, notifier = Mock(), Mock(), Mock(), Mock()
+
+        Pipeline("pipeline", [step1, step2], notifier)
+
+        failure_callback = step1.add_failure_listener.call_args[0][0]
+        failure_callback(commit)
+
+        notifier.on_failure.assert_called_once_with(commit)
+
+
+class BuildStepTests(unittest.TestCase):
+    def test_build_step_passes_calls_callbacks_for_successes(self):
+        step = BuildStep("step1")
+        callback = Mock()
+        commit = Commit()
+
+        step.add_success_listener(callback)
+        step.add_commit(commit)
+        step.succeed()
+
+        callback.assert_called_once_with(commit)
+
+    def test_build_step_passes_calls_callbacks_for_failures(self):
+        step = BuildStep("step1")
+        callback = Mock()
+        commit = Commit()
+
+        step.add_failure_listener(callback)
+        step.add_commit(commit)
+        step.fail()
+
+        callback.assert_called_once_with(commit)
+
+    def test_build_step_doesnt_call_wrong_callbacks(self):
+        step = BuildStep("step1")
+        success_callback, failure_callback = Mock(), Mock()
+        commit1, commit2 = Commit(), Commit()
+
+        step.add_success_listener(success_callback)
+        step.add_failure_listener(failure_callback)
+        step.add_commit(commit1)
+        step.fail()
+        step.add_commit(commit2)
+        step.succeed()
+
+        failure_callback.assert_called_once_with(commit1)
+        success_callback.assert_called_once_with(commit2)
