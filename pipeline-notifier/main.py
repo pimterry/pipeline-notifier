16b691956f131439bd95f7aa8aaf37f5809873bc
pimterry <pimterry@gmail.com>
2014-01-28 19:36:27 +0000

    Initial commit with working flask/cherrypy server

---
+++ pipeline-notifier/main.py
@@ -0,0 +1,25 @@
+import os
+import cherrypy
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello Worlds!'
+
+def run_server():
+    cherrypy.tree.graft(app, '/')
+
+    cherrypy.config.update({
+        'engine.autoreload_on': True,
+        'log.screen': True,
+        'server.socket_port': 8080,
+        'server.socket_host': '0.0.0.0'
+    })
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
+if __name__ == '__main__':
+    run_server()
